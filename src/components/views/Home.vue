f1943227c1d16f7e1b63d7081bf900f21bf269de
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-08-27 11:47:15 -0400

    implementando componente home

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,31 +1,77 @@
 <template>
-  <div>
-    <h1>Componente Home</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <div class="h-100 p-5 bg-light border rounded-3">
+          <h2>Pesquisar Vagas</h2>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label for="">Titulo da Vaga</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Pesquise por palavras chaves, por exemplo 'PHP','Pleno','Analista'"
+                />
+                <small class="form-text text-muted"
+                  >Informe palavras que estejam relacionadas com o título da
+                  vaga que você procura</small
+                >
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <button class="btn btn-outline-dark mt2" type="button">Buscar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Vagas abertas</p>
+          <h2>25</h2>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Profissionais Cadastradas</p>
+          <h2>125</h2>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+          <p>Visitantes online</p>
+          <h2>15</h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  created(){
-    console.log('Criado', this.teste)
+  created() {
+    console.log("Criado", this.teste);
   },
-   activated(){
-    console.log('Componente é ativado');
+  activated() {
+    console.log("Componente é ativado");
   },
-  deactivated(){
-    console.log('Componente é desativado');
+  deactivated() {
+    console.log("Componente é desativado");
   },
-  beforeUnmount(){
-    console.log('Antes de desmontar/destruir')
+  beforeUnmount() {
+    console.log("Antes de desmontar/destruir");
   },
-  unmounted(){
-    console.log('Desmontado/destruido');
-  }
+  unmounted() {
+    console.log("Desmontado/destruido");
+  },
 };
 </script>
 
 
 <style scoped>
-
 </style>
