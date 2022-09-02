05d7cc38b7aa6f3f586986d6c1bb63fe6a60d7ce
Oriovaldo Fialho <oriovaldof@gmail.com>
2022-09-02 08:10:37 -0400

    adicionando e configurando emmiti

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//importar o pacote mitt
+import mitt from 'mitt'
+
+//criar a instancia do pacote mitt
+const emitter = mitt();
+
+//inicia a instancia do vue com base no componente 
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+
+//configurar a instancia do pacote mitt como sendo uma propriedade global
+//essa propriedade estará disponivel para todas as instancias de componentes dentro do app
+app.config.globalProperties.emitter = emitter;
+
+//associar a instancia do vue com o elemento html de id app
+app.mount('#app');
